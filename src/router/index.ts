9e06c9141cecd2f16cdbdb48cb3d811d0723eeb1
Josip Novak <josip.novak@hiwe-it.com>
2023-06-27 09:50:30 +0200

    wip

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,9 @@ import routes from './routes';
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -32,6 +34,22 @@ export default route(function (/* { store, ssrContext } */) {
       process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE
     ),
   });
+
+  Router.beforeEach(() => {
+    metaAdder(
+      'og:image',
+      'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'
+    );
+  });
+
+  const metaAdder = (queryProperty: string, value: string) => {
+    // It doesn't exist so lets make a HTML element string with the info we want
+    const element =
+      `<meta property="${queryProperty}" content="${value}" />` as never;
+
+    // And insert it into the head
+    document.head.insertAdjacentHTML('beforeend', element);
+  };
 
   return Router;
 });
