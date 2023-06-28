47ac4c2272719baa052a16d8512edb8b73506943
Josip Novak <josip.novak@hiwe-it.com>
2023-06-28 12:29:43 +0200

    wip

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,14 +1,5 @@
 import { RouteRecordRaw } from 'vue-router';
 
-const metaAdder = (queryProperty: string, value: string) => {
-  // It doesn't exist so lets make a HTML element string with the info we want
-  const element =
-    `<meta property="${queryProperty}" content="${value}" />` as never;
-
-  // And insert it into the head
-  document.head.insertAdjacentHTML('beforeend', element);
-};
-
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
@@ -18,11 +9,19 @@ const routes: RouteRecordRaw[] = [
       {
         path: '/meta',
         component: () => import('pages/MetaPage.vue'),
-        beforeEnter: () =>
-          metaAdder(
-            'og:image',
-            'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'
-          ),
+        meta: {
+          title: 'Meta Page test',
+          metaTags: [
+            {
+              name: 'description',
+              content: 'Please make this work!!!!!!!!!!!',
+            },
+            {
+              property: 'og:description',
+              content: 'Please make this work!!!!!!!!!!!',
+            },
+          ],
+        },
       },
     ],
   },
