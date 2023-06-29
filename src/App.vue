9bbd942be4af03eb4c3d89645cc2dc168b127256
Josip Novak <josip.novak@hiwe-it.com>
2023-06-29 08:49:40 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
 </template>
 
 <script>
+import { useRoute } from 'vue-router';
+
 export default {
   preFetch() {
+    const route = useRoute();
+    console.log(route.params);
     console.log('running preFetch in app');
     document.head.innerHTML +=
       '<meta property="og:image" content="https://www.searchenginejournal.com/wp-content/uploads/2021/06/8-creative-ways-to-earn-or-build-links-to-your-site-60c778090d7da.png">';
